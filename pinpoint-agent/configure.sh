83fe2cc2071ebac7273c0c52bf1388c419f4dad0
Dawid Malinowski <dawidmalina@gmail.com>
2015-07-09 17:46:50 +0200

    Fix with sed and env

--- pinpoint-agent/configure.sh
+++ pinpoint-agent/configure.sh
@@ -4,4 +4,4 @@ set -x
 
 COLLECTOR_IP=${COLLECTOR_IP:-}
 
-sed -i 's/profiler.collector.ip=127.0.0.1/profiler.collector.ip=${COLLECTOR_IP}/' /opt/app/pinpoint-agent/pinpoint.config
+sed -i 's/profiler.collector.ip=127.0.0.1/profiler.collector.ip='"${COLLECTOR_IP}"'/' /opt/app/pinpoint-agent/pinpoint.config
