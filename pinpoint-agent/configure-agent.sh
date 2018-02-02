160b0d9dabfcefccc5afe11cdb473c71eeb1fcc3
Dawid Malinowski <dawidmalina@gmail.com>
2018-02-02 19:57:52 +0100

    Bump to 1.7.1

--- pinpoint-agent/configure-agent.sh
+++ pinpoint-agent/configure-agent.sh
@@ -2,14 +2,14 @@
 set -e
 set -x
 
-COLLECTOR_IP=${COLLECTOR_IP:-127.0.0.1}
+COLLECTOR_IP=${COLLECTOR_IP:-1.7.1.0.1}
 COLLECTOR_TCP_PORT=${COLLECTOR_TCP_PORT:-9994}
 COLLECTOR_UDP_STAT_LISTEN_PORT=${COLLECTOR_UDP_STAT_LISTEN_PORT:-9995}
 COLLECTOR_UDP_SPAN_LISTEN_PORT=${COLLECTOR_UDP_SPAN_LISTEN_PORT:-9996}
 
 cp -f /assets/pinpoint.config /assets/pinpoint-agent/pinpoint.config
 
-sed -i "s/profiler.collector.ip=127.0.0.1/profiler.collector.ip=${COLLECTOR_IP}/g" /assets/pinpoint-agent/pinpoint.config
+sed -i "s/profiler.collector.ip=1.7.1.0.1/profiler.collector.ip=${COLLECTOR_IP}/g" /assets/pinpoint-agent/pinpoint.config
 
 sed -i "s/profiler.collector.tcp.port=9994/profiler.collector.tcp.port=${COLLECTOR_TCP_PORT}/g" /assets/pinpoint-agent/pinpoint.config
 sed -i "s/profiler.collector.stat.port=9995/profiler.collector.stat.port=${COLLECTOR_UDP_STAT_LISTEN_PORT}/g" /assets/pinpoint-agent/pinpoint.config
