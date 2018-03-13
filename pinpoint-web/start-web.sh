987b153c91bfdef5cfea23accee421c391da6e59
Dawid Malinowski <dawidmalina@gmail.com>
2018-03-13 23:23:23 +0100

    enable application inspector

--- pinpoint-web/start-web.sh
+++ pinpoint-web/start-web.sh
@@ -6,6 +6,8 @@ CLUSTER_ENABLE=${CLUSTER_ENABLE:-false}
 CLUSTER_ZOOKEEPER_ADDRESS=${CLUSTER_ZOOKEEPER_ADDRESS:-localhost}
 CLUSTER_WEB_TCP_PORT=${CLUSTER_WEB_TCP_PORT:-9997}
 
+ENABLE_APPLICATION_INSPECTOR=${ENABLE_APPLICATION_INSPECTOR:-false}
+
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-admin}
 
 HBASE_HOST=${HBASE_HOST:-localhost}
@@ -19,6 +21,8 @@ cp /assets/hbase.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase
 
 sed -i "s/cluster.enable=true/cluster.enable=${CLUSTER_ENABLE}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-web.properties
 sed -i "s/cluster.zookeeper.address=localhost/cluster.zookeeper.address=${CLUSTER_ZOOKEEPER_ADDRESS}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-web.properties
+
+sed -i "s/config.show.applicationStat=false/config.show.applicationStat=${ENABLE_APPLICATION_INSPECTOR}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-web.properties
 
 sed -i "s/admin.password=admin/admin.password=${ADMIN_PASSWORD}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-web.properties
 
